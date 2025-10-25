c06f4411606cc40151e42ad120586ee998a85809
cedrick12gatabz <Cedrickgatabazi@gmail.com>
2025-10-25 12:19:45 +0200

    Add files via upload

---
+++ ft_atoi.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgatabaz <cgatabaz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/25 02:23:55 by cgatabaz          #+#    #+#             */
+/*   Updated: 2025/10/25 02:24:33 by cgatabaz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int	result;
+	int	sign;
+
+	result = 0;
+	sign = 1;
+	while (*str == 32 || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '-')
+		sign *= -1;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + *str - '0';
+		str++;
+	}
+	return (sign * result);
+}
+/*
+int	main()
+{
+	char	*str = " +-123ab";
+	int	result = ft_atoi(str);
+	int     res = atoi(str);
+	printf("%d\n", result);
+	printf("%d\n ", res);
+	return (0);
+}
+*/
